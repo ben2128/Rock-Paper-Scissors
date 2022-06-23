89b23e346aa11feabec5cac2278f550f50564963
ben2128 <98095321+ben2128@users.noreply.github.com>
2022-06-23 14:31:42 -0500

    Update Rock, paper, scissors.py

--- Rock, paper, scissors.py
+++ Rock, paper, scissors.py
@@ -1,29 +1,31 @@
 import random
-i=random.randint(0,2)
-a=input("Pick Rock, Paper, or Sisscors...")
-if i==0:
-    i=("Rock")
-    if a==i:
+comchoice=random.randint(0,2)
+userpick=input("Pick Rock, Paper, or Sisscors...")
+if comchoice==0:
+    comchoice=("Rock")
+    if userpick==comchoice:
         print("It's a Tie!")
-    elif a==("Paper"):
+    elif userpick==("Paper"):
         print("You win Paper covers Rock!")
-    elif a==("Sisscors"):
+    elif userpick==("Sisscors"):
         print("I win, My Rock smashes your Sisscors")
-if i==1:
-    i=('Paper')
-    if a==i:
+if comchoice==1:
+    comchoice=('Paper')
+    if userpick==comchoice:
         print("It's a Tie!")
-    elif a==("Sisscors"):
+    elif userpick==("Sisscors"):
         print("You win your Sisscors cuts my Paper!")
-    elif a==("Rock"):
+    elif userpick==("Rock"):
         print("I win, My Paper covers your Rock!")
-if i==2:
-    i=("Sisscors")
-    if a==i:
+if comchoice==2:
+    comchoice=("Sisscors")
+    if userpick==comchoice:
         print("It's a Tie!")
-    elif a==("Rock"):
+    elif userpick==("Rock"):
         print("You win your Rock smashes my Sisscors!")
-    elif a==("Paper"):
+    elif userpick==("Paper"):
         print("I win, My Sisscors cuts your Paper!")
-if a==("Joey's Cockeroach"):
+if userpick==("Joey's Cockeroach"):
     print("There is no beating that!!!")
+
+
